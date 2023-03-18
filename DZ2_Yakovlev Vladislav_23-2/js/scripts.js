bc5aa13faaa4932807dd7c56629be85acfe41299
Vlad <ykt.pc_mail@ru>
2023-03-18 16:28:04 +0600

    added homework-2

---
+++ DZ2_Yakovlev Vladislav_23-2/js/scripts.js
@@ -0,0 +1,25 @@
+const circle = document.querySelector(".circle");
+
+let count = 0;
+let topPos = 0;
+
+const circleMove = () => {
+  if (count <= 1300 && topPos == 0) {
+    count += 10;
+    circle.style.left = `${count}px`; 
+    setTimeout(circleMove, 10);
+  } else if (count >= 600 && topPos <= 600) {
+    topPos += 10;
+    circle.style.top = `${topPos}px`; 
+    setTimeout(circleMove, 10);
+  } else if (topPos >= 360 && count != 0) {
+    count -= 10;
+    circle.style.left = `${count}px`;
+    setTimeout(circleMove, 10);
+  } else if (count == 0) {
+    topPos -= 10;
+    circle.style.top = `${topPos}px`;
+    setTimeout(circleMove, 10);
+  }
+};
+circleMove();
